4486d8309e42aec09e52427504b314a98d2ed9d1
Andrey Mokriev <andrey@mokriev.ru>
2021-12-17 20:01:39 +0300

    Small improvements

--- js/app.js
+++ js/app.js
@@ -1,32 +1,28 @@
+let date;
+
+//Вывод времени
 function showTime(){
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
-  var s = date.getSeconds(); // 0 - 59
+  date = new Date();
+  let h = date.getHours(); 
+  let m = date.getMinutes(); 
+  let s = date.getSeconds();
 
   h = (h < 10) ? "0" + h : h;
   m = (m < 10) ? "0" + m : m;
   s = (s < 10) ? "0" + s : s;
 
-  var time = h + ":" + m + ":" + s;
-  document.getElementById("MyClockDisplay").innerText = time;
+  let time = h + ":" + m + ":" + s;
   document.getElementById("MyClockDisplay").textContent = time;
-
   setTimeout(showTime, 1000);
-
 }
 //Получаем день недели на русском
 function ShowDayOfWeek() {
   let daysOfWeek = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
-  let date = new Date()
-  let dayofWeek =  daysOfWeek[date.getDay()]
-
-  document.getElementById("DayOfWeekDisplay").textContent = dayofWeek;
+  document.getElementById("DayOfWeekDisplay").textContent = daysOfWeek[date.getDay()];
 }
 //Получаем номер урока
 function getLessons() {
-  let date = new Date();
-  date =  date.getDay();
+  date = date.getDay();
 
   const lessons = [
     {
@@ -35,23 +31,18 @@ function getLessons() {
     {
       0: 'Математика', 1: 'История', 2: 'Информатика', 3: 'Информатика', 4: 'Математика', 5: 'ОБЖ', 6: 'Астрономия' 
     },
-
     {
       0: 'Английский язык', 1: 'Английский язык', 2: 'Физика', 3: 'Физика', 4: 'География', 5: 'Информатика', 6: 'Информатика' 
     },
-
     {
       0: 'Математика', 1: 'Математика', 2: 'Литература', 3: 'Физика', 4: 'Физкультура', 5: 'Английский язык', 6: '' 
     },
-
     {
       0: 'Биология', 1: 'Химия', 2: 'Физкультура', 3: 'История', 4: 'Русский язык', 5: 'Литература', 6: 'Физика' 
     },
-    
     {
       0: 'История', 1: 'Математика', 2: 'Физкультура', 3: 'Математика', 4: 'Физика', 5: 'Русский язык', 6: 'Литература' 
     },
-
     {
       0: 'Нет уроков', 1: 'Нет уроков', 2: 'Нет уроков', 3: 'Нет уроков', 4: 'Нет уроков', 5: 'Нет уроков', 6: 'Нет уроков' 
     }
@@ -65,7 +56,6 @@ function getLessons() {
 }
 
 // Tasks
-
 options = document.querySelector('select');
 textarea = document.querySelector('txtarea');
 
@@ -78,10 +68,7 @@ function taskSave() {
     subject = options[i].value;
    }
   }
-
-  task = txtarea.value;
-
-  localStorage.setItem(subject,task);
+  localStorage.setItem(subject,txtarea.value);
 }
 
 //Загрузка данных
@@ -93,7 +80,6 @@ function taskLoad() {
     subject = options[i].value;
    }
   }
-
   txtarea.value = localStorage.getItem(subject);
 }
 
